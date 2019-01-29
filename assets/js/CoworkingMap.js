0455fecb7eeca340e7be0ca1be975c4033ea706c
Martin Sarsini <martin@apphancer.com>
2019-01-29 12:46:32 +0100

    Fix issue with markers randomly not displaying by moving map.loadImage() out of map.on() function; Reformat code

--- assets/js/CoworkingMap.js
+++ assets/js/CoworkingMap.js
@@ -7,11 +7,11 @@ let places = {};
 places.type = "FeatureCollection"
 places.features = berlinPlaces;
 
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
 export default class CoworkingMap extends Component {
     componentDidMount() {
-        mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-
-        const map = new mapboxgl.Map({
+        let map = new mapboxgl.Map({
             container: this.mapContainer,
             center: [13.392, 52.523],
             zoom: 12,
@@ -20,15 +20,15 @@ export default class CoworkingMap extends Component {
 
         map.addControl(new mapboxgl.NavigationControl());
 
+        map.loadImage('http://localhost:8081/build/images/marker.png', function (error, image) {
+            if (error) throw error;
+            map.addImage('marker', image);
+        });
+
         map.on('load', (e) => {
             map.addSource('places', {
                 type: 'geojson',
                 data: places
-            });
-
-            map.loadImage('http://localhost:8081/build/images/marker.png', function (error, image) {
-                if (error) throw error;
-                map.addImage('marker', image);
             });
 
             map.addLayer({
@@ -40,44 +40,39 @@ export default class CoworkingMap extends Component {
                     'icon-size': 0.25
                 }
             });
+        });
 
-            // Create a popup, but don't add it to the map yet.
-            var popup = new mapboxgl.Popup({
-                closeButton: false,
-                closeOnClick: false
-            });
+        let popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
+        });
 
-            map.on('click', 'places', function (e) {
-                map.getCanvas().style.cursor = 'pointer';
+        map.on('click', 'places', function (e) {
+            map.getCanvas().style.cursor = 'pointer';
 
-                var coordinates = e.features[0].geometry.coordinates.slice();
-                var description = e.features[0].properties.name;
+            let coordinates = e.features[0].geometry.coordinates.slice();
+            let description = e.features[0].properties.name;
 
-                // Ensure that if the map is zoomed out such that multiple
-                // copies of the feature are visible, the popup appears
-                // over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
 
-                // Populate the popup and set its coordinates
-                // based on the feature found.
-                popup.setLngLat(coordinates)
-                    .setHTML(description)
-                    .addTo(map);
-            });
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup.setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        });
 
-            map.on('mouseenter', 'places', function () {
-                map.getCanvas().style.cursor = 'pointer';
-            });
+        map.on('mouseenter', 'places', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
 
-            map.on('mouseleave', 'places', function () {
-                map.getCanvas().style.cursor = '';
-            });
-
-            setTimeout(function () {
-                map.getSource('places').setData(places);
-            }, 3000); // @todo[m]: fix issue with layer randomly displaying
+        map.on('mouseleave', 'places', function () {
+            map.getCanvas().style.cursor = '';
         });
     }
 
