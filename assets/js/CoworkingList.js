a9ebf987bbb34db8e3bc6e1ddf5da6d41a8b2dda
Martin Sarsini <martin@apphancer.com>
2019-02-11 13:57:34 +0100

    Change style for pricing area and add Configuration buttons

--- assets/js/CoworkingList.js
+++ assets/js/CoworkingList.js
@@ -10,6 +10,8 @@ export default class CoworkingList extends Component {
             <div className="full-height">
                 <div className="p-4">
                     <h1>Coworking spaces</h1>
+                    <a href="#" className="btn btn-outline-primary btn-sm"><i className="fas fa-search"></i></a>
+                    <a href="#" className="btn btn-outline-primary btn-sm"><i className="fas fa-cog"></i></a>
                     <p>{totalSpaces} spaces</p>
                 </div>
                 <ul className="list-group list-group-flush">
@@ -21,7 +23,17 @@ export default class CoworkingList extends Component {
                         >
                             <h5>{place.properties.name}</h5>
                             <p className="address">{place.properties.address.street}, {place.properties.address.postcode}, {place.properties.address.city}</p>
-                            <a href={place.properties.website} target="_blank">website</a>
+
+                            <ul className="rates list-inline">
+                                {place.properties.rates.day.length > 0 &&
+                                <li className="list-inline-item"><span>{place.properties.rates.day}€</span> Day</li>}
+                                {place.properties.rates.flex.length > 0 &&
+                                <li className="list-inline-item"><span>{place.properties.rates.flex}€</span> Flex</li>}
+                                {place.properties.rates.fixed.length > 0 &&
+                                <li className="list-inline-item"><span>{place.properties.rates.fixed}€</span> Fixed</li>}
+                            </ul>
+
+                            <a href={place.properties.website} target="_blank"><i className="fas fa-external-link-alt"></i></a>
                         </li>
                     ))}
                 </ul>
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ import {render} from 'react-dom';
 import CoworkingMap from './CoworkingMap';
 
 import '../css/app.scss';
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import "@fortawesome/fontawesome-free/scss/solid.scss";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 render(
